91041eaf42ad1eb91b430ded4f28f41dc1f584d0
Imran Hasan <imranh94@gmail.com>
2021-06-27 18:47:20 +0500

    resolved errors and login details added

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, logging, flash
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+import math
+import time
 import os
 import csv
 from os.path import join, dirname, realpath
-import math
-import time 
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'schedulify-flask'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init MySQL
+mysql = MySQL(app)
+
+@app.route('/')
 def index():
-    with open('schedule.csv') as f:
+  return render_template('index.html')
+
+@app.route('/scheduler', methods=['GET', 'POST'])
+def scheduler():
+  with open('schedule.csv') as f:
         result = [{k: v for k, v in row.items()}
             for row in csv.DictReader(f, skipinitialspace=True)]
-    return render_template('index.html', result = result)
+  return render_template('scheduler.html', result = result)
 
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -27,13 +46,11 @@ def upload():
         print("SAVED")
     else:
         print('no file')
-    return redirect(url_for('index'))
-
-
+    return redirect(url_for('scheduler'))
 
 # read csv file and save it to dictionary
-fileName = 'uploads/data.csv'
-f = open(fileName, 'r')
+fileName = 'uploads/data2.csv'
+f = open(fileName, 'r', errors="ignore")
 reader = csv.reader(f)
 coursesData = {}
 for row in reader:
@@ -42,6 +59,62 @@ for row in reader:
 # courses data by faculty name
 courses = sorted(coursesData, key=lambda x: (coursesData[x]['faculty']))
 
+# user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    # get form fields
+    username = request.form['username']
+    password_candidate = request.form['password']
+    
+    #create a cursor
+    cur = mysql.connection.cursor()
+
+    #get user by username
+    result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+    if result > 0:
+      # get stored hash
+      data = cur.fetchone()
+      password = data['password']
+
+      #compare passwords
+      if sha256_crypt.verify(password_candidate, password):
+        # Passed
+        session['logged_in'] = True
+        session['username'] = username
+        flash('You are now logged in', 'success')
+        return redirect(url_for('index'))
+      else:
+        error = 'Invalid Login'
+        return render_template('login.html', error = error)
+      #close connection
+      cur.close()
+
+    else:
+      error = 'Username not found'
+      return render_template('login.html', error = error)
+
+  return render_template('login.html')
+
+# Check if user logged in
+def is_logged_in(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      flash('Unauthorized, please login', 'danger')
+      return redirect(url_for('login'))
+  return wrap
+
+# logout
+@app.route('/logout')
+@is_logged_in 
+def logout():
+  session.clear()
+  flash('You are now logged out', 'success')
+  return redirect(url_for('login'))
 
 # initialize variables
 board = []
@@ -160,23 +233,21 @@ def solve(board, courses, k):
 
 @app.route('/generate', methods=['GET'])
 def generate():
-
     started = time.time()
     create_board(len(courses))
     solve(board, courses, k)
-    print(result)
+    print_board()
     print("Solution Found: ", solve(board, courses, k))
     completed = time.time()
     print("Time Taken: ", completed - started)
     toCSV = result
     keys = toCSV[0].keys()
-    print(keys)
     with open('schedule.csv', 'w', newline='')  as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(toCSV)
-    return redirect(url_for('index'))
-
+    return redirect(url_for('scheduler'))
 
 if __name__ == '__main__':
+    app.secret_key = 'secret123'
     app.run(debug=True)
